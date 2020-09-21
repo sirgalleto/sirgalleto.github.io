f9f5f4e0d8363ff2a93e29d31f1344411494db02
Sebastián Osorio <sosorio@skycatch.com>
2020-09-21 05:20:58 -0500

    📝 Fixes

--- public/templates/templates.precompiled.js
+++ public/templates/templates.precompiled.js
@@ -9,7 +9,7 @@ templates['allProjects'] = template({"1":function(container,depth0,helpers,parti
     };
 
   return "  <div class=\"gtx-project-box__container\">\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":3,"column":4},"end":{"line":9,"column":13}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":4,"column":4},"end":{"line":10,"column":13}}})) != null ? stack1 : "")
     + "  </div>\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -20,13 +20,13 @@ templates['allProjects'] = template({"1":function(container,depth0,helpers,parti
     };
 
   return "      <a class=\"gtx-project-box\" href=\"project.html?project="
-    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":4,"column":60},"end":{"line":4,"column":68}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":5,"column":60},"end":{"line":5,"column":68}}}) : helper)))
     + "\" data-aos=\"fade-up\">\n        <aside class=\"gtx-project-box__index\">/ "
-    + alias4((lookupProperty(helpers,"increment")||(depth0 && lookupProperty(depth0,"increment"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"index") : depth0),{"name":"increment","hash":{},"data":data,"loc":{"start":{"line":5,"column":48},"end":{"line":5,"column":67}}}))
+    + alias4((lookupProperty(helpers,"increment")||(depth0 && lookupProperty(depth0,"increment"))||alias2).call(alias1,(depth0 != null ? lookupProperty(depth0,"index") : depth0),{"name":"increment","hash":{},"data":data,"loc":{"start":{"line":6,"column":48},"end":{"line":6,"column":67}}}))
     + "</aside>\n        <span class=\"gtx-project-box__tag\">"
-    + alias4(((helper = (helper = lookupProperty(helpers,"type") || (depth0 != null ? lookupProperty(depth0,"type") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data,"loc":{"start":{"line":6,"column":43},"end":{"line":6,"column":51}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"type") || (depth0 != null ? lookupProperty(depth0,"type") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data,"loc":{"start":{"line":7,"column":43},"end":{"line":7,"column":51}}}) : helper)))
     + "</span>\n        <h2 class=\"gtx-project-box__title\">"
-    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":7,"column":43},"end":{"line":7,"column":51}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":8,"column":43},"end":{"line":8,"column":51}}}) : helper)))
     + "</h2>\n      </a>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -36,7 +36,7 @@ templates['allProjects'] = template({"1":function(container,depth0,helpers,parti
         return undefined
     };
 
-  return ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"projectsSection") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":1,"column":0},"end":{"line":11,"column":9}}})) != null ? stack1 : "");
+  return ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"projectsSection") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":0},"end":{"line":12,"column":9}}})) != null ? stack1 : "");
 },"useData":true});
 templates['footer'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<footer class=\"gtx-section gtx-section--primary gtx-section--footer\">\n  <img\n    src=\"public/galleto-isotype-white.png\"\n    alt=\"Galleto isotype\"\n    class=\"gtx-isotype\"\n  />\n  <span class=\"gtx-text gtx-text--right\">\n    © 2020. All rights reserved.Galleto\n  </span>\n  <div class=\"gtx-flex-container gtx-navbar--footer__container gtx-flex-column\">\n    <span class=\"gtx-text\">Become a friend</span>\n    <nav class=\"gtx-navbar gtx-navbar--footer\">\n      <ul class=\"gtx-navbar__options\">\n        <li>\n          <a href=\"https://twitter.com/sirgalleto\">Twitter</a>\n        </li>\n        <li>\n          <a href=\"https://www.instagram.com/sirgalleto/\">Instagram</a>\n        </li>\n        <li>\n          <a href=\"http://github.com/sirgalleto/\"> Github</a>\n        </li>\n      </ul>\n    </nav>\n  </div>\n</footer>";
@@ -67,13 +67,13 @@ templates['projectsPreview'] = template({"1":function(container,depth0,helpers,p
     };
 
   return "    <a class=\"gtx-project-box\" href=\"project.html?project="
-    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":3,"column":58},"end":{"line":3,"column":66}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":4,"column":58},"end":{"line":4,"column":66}}}) : helper)))
     + "\" data-aos=\"fade-up\">\n      <aside class=\"gtx-project-box__index\">/ "
-    + alias4((lookupProperty(helpers,"increment")||(depth0 && lookupProperty(depth0,"increment"))||alias2).call(alias1,(data && lookupProperty(data,"index")),{"name":"increment","hash":{},"data":data,"loc":{"start":{"line":4,"column":46},"end":{"line":4,"column":66}}}))
+    + alias4((lookupProperty(helpers,"increment")||(depth0 && lookupProperty(depth0,"increment"))||alias2).call(alias1,(data && lookupProperty(data,"index")),{"name":"increment","hash":{},"data":data,"loc":{"start":{"line":5,"column":46},"end":{"line":5,"column":66}}}))
     + "</aside>\n      <span class=\"gtx-project-box__tag\">"
-    + alias4(((helper = (helper = lookupProperty(helpers,"type") || (depth0 != null ? lookupProperty(depth0,"type") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data,"loc":{"start":{"line":5,"column":41},"end":{"line":5,"column":49}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"type") || (depth0 != null ? lookupProperty(depth0,"type") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data,"loc":{"start":{"line":6,"column":41},"end":{"line":6,"column":49}}}) : helper)))
     + "</span>\n      <h2 class=\"gtx-project-box__title\">"
-    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":6,"column":41},"end":{"line":6,"column":49}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":7,"column":41},"end":{"line":7,"column":49}}}) : helper)))
     + "</h2>\n    </a>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -84,7 +84,7 @@ templates['projectsPreview'] = template({"1":function(container,depth0,helpers,p
     };
 
   return "<div class=\"gtx-project-box__container\">\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"projects") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":2},"end":{"line":8,"column":11}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"projects") : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":3,"column":2},"end":{"line":9,"column":11}}})) != null ? stack1 : "")
     + "</div>";
 },"useData":true});
 })();
