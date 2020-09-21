3a61769deca000371a4d81e99e8d367d8f741dfd
Sebastián Osorio <sosorio@skycatch.com>
2020-09-21 01:31:13 -0500

    ✨ Add contact form

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,7 +1,29 @@
 (function () {
+  const EMAIL = "svzosorio@gmail.com";
+  const SUBJECT = "Hello from your page!";
+  let messageTextBox;
+  let sendAnchor;
+
   function start() {
     window.helpers.renderTemplate("header", "header");
     window.helpers.renderTemplate("footer", "footer");
+
+    messageTextBox = document.getElementById("message");
+    sendAnchor = document.getElementById("send");
+    contactForm = document.getElementById("contact-form");
+    messageTextBox.onchange = onMesage;
+
+    sendAnchor.href = buildMailTo();
+  }
+
+  // Builds the mailto url with the default email and subject
+  function buildMailTo(body = "") {
+    return `mailto:${EMAIL}?subject=${SUBJECT}&body=${body}`;
+  }
+
+  // When the textbox changes, we update the link href with the new mailto that contains the message
+  function onMesage(e) {
+    sendAnchor.href = buildMailTo(messageTextBox.value);
   }
 
   window.onload = start;
