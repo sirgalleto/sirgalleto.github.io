3ba8005047d2a39aa94d54d8d32ef2ad2d80c5ac
Sebastián Osorio <sosorio@skycatch.com>
2020-10-21 21:51:36 -0500

    ♻️ Refactor to ESM

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -1,42 +1,41 @@
-// Libraries: AOS
+import AOS from "aos";
+import { renderTemplate } from "./helpers";
+import "aos/dist/aos.css";
+import projects from "../data/projects.json";
 
-(function () {
-  const fetch = window.fetch;
-  const ROW_SIZE = 4;
+const fetch = window.fetch;
+const ROW_SIZE = 4;
 
-  function start() {
-    window.helpers.renderTemplate("header", "header");
-    window.helpers.renderTemplate("footer", "footer");
-    // Creates an array of arrays, each array contains a ROW_SIZE elements
-    // Is used for separate the rows of projects
-    const projectsSection = window.data.projects.reduce(
-      (accumulator, value, index) => {
-        const lastArray = accumulator[accumulator.length - 1];
-        const item = {
-          index,
-          ...value,
-        };
+function start() {
+  // Creates an array of arrays, each array contains a ROW_SIZE elements
+  // Is used for separate the rows of projects
+  const projectsSection = projects.reduce(
+    (accumulator, value, index) => {
+      const lastArray = accumulator[accumulator.length - 1];
+      const item = {
+        index,
+        ...value,
+      };
 
-        // Insers an array until is full
-        if (lastArray.length < ROW_SIZE) {
-          lastArray.push(item);
-        } else {
-          accumulator.push([item]);
-        }
+      // Insers an array until is full
+      if (lastArray.length < ROW_SIZE) {
+        lastArray.push(item);
+      } else {
+        accumulator.push([item]);
+      }
 
-        return accumulator;
-      },
-      [[]]
-    );
+      return accumulator;
+    },
+    [[]]
+  );
 
-    window.helpers.renderTemplate("projects", "allProjects", {
-      projectsSection,
-    });
+  renderTemplate("projects", "allProjects", {
+    projectsSection,
+  });
 
-    AOS.init({
-      offset: 200,
-    });
-  }
+  AOS.init({
+    offset: 200,
+  });
+}
 
-  window.onload = start;
-})();
+window.onload = start;
