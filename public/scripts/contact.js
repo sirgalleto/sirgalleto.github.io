63bb9761edfde9be6690c9f3e0ea814fd3d1382b
Sebastián Osorio <sosorio@skycatch.com>
2020-10-21 22:37:01 -0500

    🔍 Add description to page

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,30 +1,25 @@
-(function () {
-  const EMAIL = "svzosorio@gmail.com";
-  const SUBJECT = "Hello from your page!";
-  let messageTextBox;
-  let sendAnchor;
+const EMAIL = "svzosorio@gmail.com";
+const SUBJECT = "Hello from your page!";
+let messageTextBox;
+let sendAnchor;
 
-  function start() {
-    window.helpers.renderTemplate("header", "header");
-    window.helpers.renderTemplate("footer", "footer");
+function start() {
+  messageTextBox = document.getElementById("message");
+  sendAnchor = document.getElementById("send");
+  contactForm = document.getElementById("contact-form");
+  messageTextBox.onchange = onMesage;
 
-    messageTextBox = document.getElementById("message");
-    sendAnchor = document.getElementById("send");
-    contactForm = document.getElementById("contact-form");
-    messageTextBox.onchange = onMesage;
+  sendAnchor.href = buildMailTo();
+}
 
-    sendAnchor.href = buildMailTo();
-  }
+// Builds the mailto url with the default email and subject
+function buildMailTo(body = "") {
+  return `mailto:${EMAIL}?subject=${SUBJECT}&body=${body}`;
+}
 
-  // Builds the mailto url with the default email and subject
-  function buildMailTo(body = "") {
-    return `mailto:${EMAIL}?subject=${SUBJECT}&body=${body}`;
-  }
+// When the textbox changes, we update the link href with the new mailto that contains the message
+function onMesage(e) {
+  sendAnchor.href = buildMailTo(messageTextBox.value);
+}
 
-  // When the textbox changes, we update the link href with the new mailto that contains the message
-  function onMesage(e) {
-    sendAnchor.href = buildMailTo(messageTextBox.value);
-  }
-
-  window.onload = start;
-})();
+window.onload = start;
